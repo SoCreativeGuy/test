f2af772d6a8fbbff882f79c4bf383700501618cb
SoCreativeGuy <127413197+SoCreativeGuy@users.noreply.github.com>
2024-07-03 15:50:34 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,5 +1,12 @@
 ﻿#include <stdio.h>
-int main() {
-print("Hello, world!");
-return 0;
+int main() 
+{
+  print("Hello, world!");
+    
+  for(i = 0; i<10; i++)
+  {
+    print("I see the new world in GitHub!");
+  }
+    
+  return 0;
 }
